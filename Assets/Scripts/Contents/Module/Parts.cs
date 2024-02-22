9f8a0bc866d757d3d3a8d660e87840f56fed1aef
JY-LemongO <122505119+JY-LemongO@users.noreply.github.com>
2024-02-22 19:57:43 +0900

    Remove - 불필요 스크립트 삭제

--- Assets/Scripts/Contents/Module/Parts.cs
+++ Assets/Scripts/Contents/Module/Parts.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parts : MonoBehaviour, ILowerParts
+public class Parts : MonoBehaviour, IParts
 {
     protected Module _module;
 
--- Assets/Scripts/Interface/IParts.cs
+++ Assets/Scripts/Interface/IParts.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface ILowerParts
+public interface IParts
 {
     public void Move();
     public void Look();
